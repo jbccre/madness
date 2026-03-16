530e1d8214db9c5b6702b413ff4f5c53421afe16
jrbnnett@memphis.edu <jrbnnett@memphis.edu>
2026-03-15 20:34:36 -0500

    update womens dashboard

--- run_men.R
+++ run_men.R
@@ -1,4 +1,4 @@
 men <- TRUE
 test_envir <- TRUE #set to FALSE in prod
-qualtrics_simulate <- TRUE #if TRUE, creates simulated qualtrics data (for test environment only)
+qualtrics_simulate <- FALSE #if TRUE, creates simulated qualtrics data (for test environment only)
 source("madness2026.R")
