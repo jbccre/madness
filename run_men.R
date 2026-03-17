8d396c5fbd20ee16ec387789977e6ded49cf1b64
jrbnnett@memphis.edu <jrbnnett@memphis.edu>
2026-03-17 18:18:55 -0500

    let the madness begin!

--- run_men.R
+++ run_men.R
@@ -1,4 +1,4 @@
 men <- TRUE
-test_envir <- TRUE #set to FALSE in prod
+test_envir <- FALSE #set to FALSE in prod
 qualtrics_simulate <- FALSE #if TRUE, creates simulated qualtrics data (for test environment only)
 source("madness2026.R")
